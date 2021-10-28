bdbfb7ff8cdf0e1c35a326d81a1221fb3d59fe2a
farzanapomy <farzanapomy56@gmail.com>
2021-10-29 03:28:51 +0600

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express=require('express');
 const app=express();
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config()
 const port=process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fjoai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
 
 app.get('/',(req,res)=>{
     res.send('server is live');
@@ -9,3 +18,5 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log("server is running from port ",port);
 })
+
+
